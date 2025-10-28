84f8c6b223e32c2ac5d14272543c168e8a4c6521
Dovydas Valančius <dovydas.valancius@gmail.com>
2025-10-28 07:31:59 +0200

    - Created: iotcore app for handling iot data and core iot functionality. - Created: controlhub app for handling UI nuances - Created: auradash for iot data displaying and analysis

---
+++ auradash/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AuradashConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'auradash'
---
+++ controlhub/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ControlhubConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'controlhub'
